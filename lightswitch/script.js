0e1e9dcb6308fa892d71f1617dde28c0698a70ff
AmarNagim <89778585+AmarNagim@users.noreply.github.com>
2021-11-01 13:36:31 +0100

    finished-lightswitch

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -1,10 +1,29 @@
-var btn = document.createElement('button');
-btn.style.margin = '10px';
-btn.innerHTML = '...';
-document.body.appendChild(btn);
 
 // schijf hier tussen je code
+const button = document.getElementById("button");
+
+function myFunction() {
+    if (document.getElementById("light").className == "light on") {
+        document.getElementById("light").className = "light";       
+        button.innerText= "Click to turn on";
+
+    } else {
+        document.getElementById("light").className = "light on";
+        button.innerText= "Click to turn off";
 
 
+    }
+  }  
+
+// const button = document.getElementById("button");
+
+// button.addEventListener("click", ()=>{
+
+//     if(button.innerText === "click to turn on"){
+//         button.innerText = "turn on";
+//     }else{
+//         button.innerText= "click to turn turn";
+//     }
+// });
 
 // schijf hier tussen je code
